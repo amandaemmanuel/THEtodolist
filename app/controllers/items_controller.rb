2825a615309e9afa7e0f9f20c168436bcba14162
Amanda Emmanuel <amandaemmanuel@Amandas-Air-2.phub.net.cable.rogers.com>
2021-03-05 16:31:43 -0500

    optimized creating new item (removed list ID lookup)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,20 +1,19 @@
 class ItemsController < ApplicationController
-    #create method
+    #create a new item
     def create
-        # action to create a new item
-        # when creating an item, need to understand what list ID it's being associated to
-        @list = List.find_by(id: params[:list_id])
-        @item = @list.items.create(item_params)
-        redirect_to @list
+        # action to create a new item -- when creating the todo item, pass the list ID as a reference (without a lookup)
+        @item = Item.create(item_params)
+        # redirect_to list show page
+        redirect_to "/lists/#{params[:list_id]}"
     end
 
     private
 
-    def set_list
-        @list = List.find(params[:list_id])
-    end
-
     def item_params
-        params[:item].permit(:description)
+        # making the 'items hash' take in the user form info
+        item = params[:item].permit(:description, :status)
+        listID = params[:list_id] #variable to store the listid
+        list_id_hash = { list_id: listID } #making a new hash with the list id
+        item.merge(list_id_hash) # merge the 2 hashes together, so all item info is in 1 hash
     end 
 end
